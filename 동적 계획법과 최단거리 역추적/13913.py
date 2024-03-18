1fa6bd1a900057e82a4357cd86cb5fd644f7869a
조나희 <105144666+Nahhh-j@users.noreply.github.com>
2024-03-18 10:58:55 +0900

    update

--- 동적 계획법과 최단거리 역추적/13913.py
+++ 동적 계획법과 최단거리 역추적/13913.py
@@ -12,3 +12,34 @@
 첫째 줄에 수빈이가 동생을 찾는 가장 빠른 시간을 출력한다.
 둘째 줄에 어떻게 이동해야 하는지 공백으로 구분해 출력한다.
 '''
+
+from collections import deque
+
+def path(x):
+    arr = []
+    temp = x
+    for _ in range(dist[x]+1):
+        arr.append(temp)
+        temp = move[temp]
+    print(' '.join(map(str, arr[::-1])))
+
+def bfs():
+    q = deque()
+    q.append(N)
+    while q:
+        x = q.popleft()
+        if x == K:
+            print(dist[x])
+            path(x)
+            return x
+        for i in (x+1, x-1, 2*x):
+            if 0<=i<=100000 and dist[i]==0:
+                q.append(i)
+                dist[i] = dist[x] + 1
+                move[i] = x
+
+N, K = map(int, input().split())
+dist = [0]*100001
+move = [0]*100001
+
+bfs()
