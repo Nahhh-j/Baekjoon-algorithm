09366ca5921e02c45c373eba102fe7e6cd893e1a
조나희 <105144666+Nahhh-j@users.noreply.github.com>
2024-04-08 08:12:58 +0900

    update

---
+++ 조합론/11050.py
@@ -0,0 +1,26 @@
+# 이항 계수 1
+
+'''
+문제
+자연수 \(N\)과 정수 \(K\)가 주어졌을 때 이항 계수 
+\(\binom{N}{K}\)를 구하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 \(N\)과 \(K\)가 주어진다. (1 ≤ \(N\) ≤ 10, 0 ≤ \(K\) ≤ \(N\))
+
+출력
+ 
+\(\binom{N}{K}\)를 출력한다.
+'''
+
+input = sys.stdin.readline
+
+N, K = map(int, input().split())
+
+def factorial(n):
+  if n == 0 or n == 1:
+    return 1
+  else:
+    return n * factorial(n-1)
+
+print(factorial(N) // (factorial(N-K) * factorial(K)))
