0786bfbe781c0bc6dee0920e9f92c4e01af52f27
조나희 <105144666+Nahhh-j@users.noreply.github.com>
2024-04-10 10:22:15 +0900

    update

--- 백트래킹/15649.py
+++ 백트래킹/15649.py
@@ -12,3 +12,25 @@
 한 줄에 하나씩 문제의 조건을 만족하는 수열을 출력한다. 중복되는 수열을 여러 번 출력하면 안되며, 각 수열은 공백으로 구분해서 출력해야 한다.
 수열은 사전 순으로 증가하는 순서로 출력해야 한다.
 '''
+
+def dfs():
+    if len(s) == m:
+        print(' '.join(map(str, s)))
+        return
+    for i in range(1, n+1):
+        if visited[i]:
+            continue
+        visited[i] = True
+        s.append(i)
+        dfs()
+        s.pop()
+        print(s)
+        print(visited)
+        visited[i] = False
+            
+
+n, m = map(int, input().split())
+s = []
+visited = [False] * (n+1)
+
+dfs()
