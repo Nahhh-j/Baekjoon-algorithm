1951c8cedf64e0315f470a560d5461482ff96f11
조나희 <105144666+Nahhh-j@users.noreply.github.com>
2024-04-12 08:48:38 +0900

    update

---
+++ 백트래킹/9663.py
@@ -0,0 +1,41 @@
+# N-Queen
+
+'''
+문제
+N-Queen 문제는 크기가 N × N인 체스판 위에 퀸 N개를 서로 공격할 수 없게 놓는 문제이다.
+N이 주어졌을 때, 퀸을 놓는 방법의 수를 구하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 N이 주어진다. (1 ≤ N < 15)
+
+출력
+첫째 줄에 퀸 N개를 서로 공격할 수 없게 놓는 경우의 수를 출력한다.
+'''
+
+n = int(input())
+
+ans = 0
+row = [0] * n
+
+def is_promising(x):
+    for i in range(x):
+        if row[x] == row[i] or abs(row[x] - row[i]) == abs(x - i):
+            return False
+    
+    return True
+
+def n_queens(x):
+    global ans
+    if x == n:
+        ans += 1
+        return
+
+    else:
+        for i in range(n):
+            # [x, i]에 퀸을 놓겠다.
+            row[x] = i
+            if is_promising(x):
+                n_queens(x+1)
+
+n_queens(0)
+print(ans)
