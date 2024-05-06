3db115dd0e3fac1c997c5397910894b1294faf1d
조나희 <105144666+Nahhh-j@users.noreply.github.com>
2024-05-07 08:06:30 +0900

    update

--- 1697.py
+++ 1697.py
@@ -11,3 +11,23 @@
 출력
 수빈이가 동생을 찾는 가장 빠른 시간을 출력한다.
 '''
+
+import sys
+from collections import deque
+
+def bfs(v):
+    q = deque([v])
+    while q:
+        v = q.popleft()
+        if v == k:
+            return array[v]
+        for next_v in (v-1, v+1, 2*v):
+            if 0 <= next_v < MAX and not array[next_v]:
+                array[next_v] = array[v] + 1
+                q.append(next_v)
+
+
+MAX = 100001
+n, k = map(int, sys.stdin.readline().split())
+array = [0] * MAX
+print(bfs(n))
