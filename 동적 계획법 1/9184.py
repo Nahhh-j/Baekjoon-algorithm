0218e028efcaad32781ee8187f1a28049f87ae70
조나희 <105144666+Nahhh-j@users.noreply.github.com>
2024-05-12 13:26:54 +0900

    update

--- 동적 계획법 1/9184.py
+++ 동적 계획법 1/9184.py
@@ -26,3 +26,27 @@ a, b, c가 주어졌을 때, w(a, b, c)를 출력하는 프로그램을 작성�
 출력
 입력으로 주어진 각각의 a, b, c에 대해서, w(a, b, c)를 출력한다.
 '''
+
+import sys
+input = sys.stdin.readline
+
+def w(a, b, c):
+    if a <= 0 or b<= 0 or c<=0:
+        return 1
+    if a > 20 or b > 20 or c > 20:
+        return w(20, 20, 20)
+    if dp[a][b][c]:
+        return dp[a][b][c]
+    if a<b<c:
+        dp[a][b][c] = w(a,b,c-1)+w(a,b-1,c-1)-w(a, b-1, c)
+        return dp[a][b][c]
+    dp[a][b][c] = w(a-1, b, c)+w(a-1,b-1,c)+w(a-1,b,c-1)-w(a-1,b-1,c-1)
+    return dp[a][b][c]
+
+dp = [[[0]*(21) for _ in range(21)] for _ in range(21)]
+
+while 1:
+    a, b, c = map(int, input().split())
+    if a==-1 and b==-1 and c==-1:
+        break
+    print(f'w({a}, {b}, {c}) = {w(a,b,c)}')
