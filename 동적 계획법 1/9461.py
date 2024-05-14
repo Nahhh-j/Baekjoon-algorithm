b3d1d46a2ecedaacdfed5ac137e244a2688b70c4
조나희 <105144666+Nahhh-j@users.noreply.github.com>
2024-05-14 09:39:11 +0900

    update

--- 동적 계획법 1/9461.py
+++ 동적 계획법 1/9461.py
@@ -12,3 +12,19 @@ N이 주어졌을 때, P(N)을 구하는 프로그램을 작성하시오.
 출력
 각 테스트 케이스마다 P(N)을 출력한다.
 '''
+
+import sys
+input = sys.stdin.readline
+arr = [0, 1, 1, 1] + [0 for x in range(97)]
+
+def func(x):
+  if arr[x]:
+    return arr[x]
+  else:
+    arr[x] = func(x-2) + func(x-3)
+    return arr[x]
+
+t = int(input())
+for _ in range(t):
+  n = int(input())
+  print(func(n))
