5f1fccc15bb7ad306be17a00252a27543ed8ee44
조나희 <105144666+Nahhh-j@users.noreply.github.com>
2024-06-09 19:08:52 +0900

    update

--- 1520.py
+++ 1520.py
@@ -13,3 +13,31 @@
 출력
 첫째 줄에 이동 가능한 경로의 수 H를 출력한다. 모든 입력에 대하여 H는 10억 이하의 음이 아닌 정수이다.
 '''
+
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(10 ** 9)
+
+m, n = map(int, input().split())
+arr = [list(map(int, input().split())) for i in range(m)]
+dp = [[-1]*n for i in range(m)]
+dx = [0, 1, 0, -1]
+dy = [1, 0 , -1, 0]
+
+def is_range(x, y, now):
+    return 0 <= x < m and 0 <= y < n and arr[x][y] < now
+
+def solution(x, y):
+    if x == m-1 and y == n-1:
+        return 1
+    if dp[x][y] == -1:
+        dp[x][y] = 0
+        for i in range(4):
+            dr_x, dr_y = x + dx[i], y + dy[i]
+            if is_range(dr_x, dr_y, arr[x][y]):
+                dp[x][y] += solution(dr_x, dr_y)
+    
+    return dp[x][y]
+        
+    
+print(solution(0, 0))
