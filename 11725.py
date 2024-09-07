121027b4a47c367ab4c4ce2dbd7ea3f43301c35d
조나희 <105144666+Nahhh-j@users.noreply.github.com>
2024-09-07 11:50:58 +0900

    update

---
+++ 11725.py
@@ -0,0 +1,38 @@
+# 트리의 부모 찾기
+
+'''
+문제
+루트 없는 트리가 주어진다. 이때, 트리의 루트를 1이라고 정했을 때, 각 노드의 부모를 구하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 노드의 개수 N (2 ≤ N ≤ 100,000)이 주어진다. 둘째 줄부터 N-1개의 줄에 트리 상에서 연결된 두 정점이 주어진다.
+
+출력
+첫째 줄부터 N-1개의 줄에 각 노드의 부모 노드 번호를 2번 노드부터 순서대로 출력한다.
+'''
+
+import sys
+sys.setrecursionlimit(10**6)
+n = int(sys.stdin.readline())
+
+graph = [[] for i in range(n+1)]
+
+for i in range(n-1):
+    a, b = map(int, sys.stdin.readline().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+visited = [0]*(n+1)
+
+arr = []
+
+def dfs(s):
+    for i in graph[s]:
+        if visited[i] == 0:
+            visited[i] = s
+            dfs(i)
+
+dfs(1)
+
+for x in range(2, n+1):
+    print(visited[x])
