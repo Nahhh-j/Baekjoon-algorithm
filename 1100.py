ff5073e943e1ded23837a51f2d102c047e41a7d7
조나희 <105144666+Nahhh-j@users.noreply.github.com>
2024-09-18 12:35:29 +0900

    update

--- 1100.py
+++ 1100.py
@@ -10,3 +10,16 @@
 출력
 첫째 줄에 문제의 정답을 출력한다.
 '''
+
+S = []
+count = 0
+
+for _ in range(8):
+    S.append(input())
+for i in range(8):
+    for j in range(8):
+        if i % 2 == 1 and j % 2 == 1 and S[i][j] == "F":
+            count += 1
+        if i % 2 == 0 and j % 2 == 0 and S[i][j] == "F":
+            count += 1
+print(count)
