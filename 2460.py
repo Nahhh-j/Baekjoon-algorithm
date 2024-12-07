139c60fb2c548c47d30724906f732c7f82e56f56
조나희 <105144666+Nahhh-j@users.noreply.github.com>
2024-12-07 14:48:38 +0900

    update

--- 2460.py
+++ 2460.py
@@ -32,3 +32,12 @@
 첫째 줄에 최대 사람 수를 출력한다.  
 '''
 
+passenger = 0
+max_passenger = 0
+
+for _ in range(10):
+    out_train, in_train  = map(int, input().split()) 
+    passenger += in_train - out_train 
+    max_passenger = max(passenger, max_passenger) 
+    
+print(max_passenger)
