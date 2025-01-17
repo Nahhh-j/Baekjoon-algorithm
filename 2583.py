01dcbf3db7577c9b1cbaf9ef292662d2ab79fe3a
조나희 <105144666+Nahhh-j@users.noreply.github.com>
2025-01-17 09:04:51 +0900

    update

--- 2583.py
+++ 2583.py
@@ -14,3 +14,45 @@ M, N과 K 그리고 K개의 직사각형의 좌표가 주어질 때, K개의 직
 출력
 첫째 줄에 분리되어 나누어지는 영역의 개수를 출력한다. 둘째 줄에는 각 영역의 넓이를 오름차순으로 정렬하여 빈칸을 사이에 두고 출력한다.
 '''
+
+from collections import deque
+
+M, N, K = map(int, input().split())
+graph = [[0]*N for _ in range(M)]
+
+
+def bfs(i, j):
+    q = deque()
+    q.append((i, j))
+    dx = [0, 0, -1, 1]
+    dy = [1, -1, 0, 0]
+    cnt = 1
+    while q:
+        y, x = q.popleft()
+        for k in range(4):
+            ny = y+dy[k]
+            nx = x+dx[k]
+            if (0 <= ny < M) and (0 <= nx < N) and graph[ny][nx] == 0:
+                graph[ny][nx] = 1
+                q.append((ny, nx))
+                cnt += 1
+    return cnt
+
+
+for _ in range(K):
+    x1, y1, x2, y2 = map(int, input().split())
+    for i in range(y1, y2):
+        for j in range(x1, x2):
+            graph[i][j] += 1
+
+result = []
+
+for i in range(M):
+    for j in range(N):
+        if graph[i][j] == 0:
+            graph[i][j] += 1
+            result.append(bfs(i, j))
+
+print(len(result))
+print(*sorted(result))
+출처: https://tooo1.tistory.com/487 [개발자 퉁이리:티스토리]
