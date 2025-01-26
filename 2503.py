48e69513ab18c4909058872184bb821a01d4f555
조나희 <105144666+Nahhh-j@users.noreply.github.com>
2025-01-26 22:52:43 +0900

    update

--- 2503.py
+++ 2503.py
@@ -38,3 +38,19 @@
 출력
 첫 줄에 영수가 생각하고 있을 가능성이 있는 답의 총 개수를 출력한다.
 '''
+
+a=[]
+for i in range(1, 10):
+    for j in range(1, 10):
+        for k in range(1, 10):
+            if i!=j and j!=k and k!=i:
+                a.append([i, j, k])
+for _ in range(int(input())):
+    b, c, d=map(int, input().split())
+    b=[b//100, (b//10)%10, b%10]
+    e=[]
+    for i in a:
+        if int(b[0]==i[0])+int(b[1]==i[1])+int(b[2]==i[2])==c and int(b[0] in i[1:])+int(b[1] in [i[0], i[2]])+int(b[2] in i[:2])==d:
+            e.append(i)
+    a=e[:]
+print(len(a))
