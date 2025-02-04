1a42f675ab3a148096482cf62c468af0be09b749
조나희 <105144666+Nahhh-j@users.noreply.github.com>
2025-02-04 09:14:51 +0900

    update

--- 2600.py
+++ 2600.py
@@ -18,3 +18,29 @@
 출력
 각 5개의 k1, k2 경우에 대하여 그에 대응되는 승자(A 또는 B)를 각각 한 줄에 하나씩 차례대로 다섯 개를 출력해야 한다.
 '''
+
+def win(p, q):
+    if dp[p][q] >= 0:
+        return dp[p][q]
+
+    for i in range(3):
+        if b[i] <= p and not win(p - b[i], q):
+            dp[p][q] = 1
+            return dp[p][q]
+
+    for j in range(3):
+        if b[j] <= q and not win(p, q - b[j]):
+            dp[p][q] = 1
+            return dp[p][q]
+
+    dp[p][q] = 0
+    return dp[p][q]
+
+dp = [[-1 for _ in range(501)] for _ in range(501)]
+b = list(map(int, input().split()))
+for _ in range(5):
+    k1, k2 = map(int, input().split())
+    if win(k1, k2):
+        print("A")
+    else:
+        print("B")
