eec413d57645efc726a4a22aaf24837f7801785b
조나희 <105144666+Nahhh-j@users.noreply.github.com>
2025-03-01 09:28:02 +0900

    update

--- 2608.py
+++ 2608.py
@@ -27,3 +27,39 @@ MCMXL = 1000 + 900 + 40 = 1940
 출력
 입력으로 주어진 두 수를 더한 값을 첫째 줄에 아라비아숫자로 출력하고 둘째 줄에는 로마 숫자로 출력한다.
 '''
+
+order_lst = ['I', 'V', 'X', 'L', 'C', 'D', 'M']
+value_lst = [1, 5, 10, 50, 100, 500, 1000]
+
+alhpa_to_value = {i:j for i,j in  zip(order_lst, value_lst)}
+value_to_alpha = {i:j for i,j in  zip(value_lst, order_lst)}
+
+a = input().strip()
+b = input().strip()
+
+
+value = 0
+for v in [a,b]:
+    value += alhpa_to_value[v[-1]]
+    for i in range(len(v)-1):
+        if alhpa_to_value[v[i]] < alhpa_to_value[v[i+1]]:
+            value -= alhpa_to_value[v[i]]
+        else:
+            value += alhpa_to_value[v[i]]
+
+print(value)
+
+for v in value_lst[::-1]:
+    while value>=v:
+        if str(value)[0] == "4":
+            value -= 4*v 
+            print(value_to_alpha[v], end="")
+            print(value_to_alpha[v*5], end="")
+        elif str(value)[0] == "9":
+            v //= 5
+            value -= 9*v 
+            print(value_to_alpha[v], end="")
+            print(value_to_alpha[v*10], end="")
+        else:
+            value-=v
+            print(value_to_alpha[v], end="")
