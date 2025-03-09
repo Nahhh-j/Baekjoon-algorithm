cadea73d17294176aab42321549a0e0952ef662d
조나희 <105144666+Nahhh-j@users.noreply.github.com>
2025-03-09 16:05:23 +0900

    update

--- 2669.py
+++ 2669.py
@@ -11,3 +11,17 @@
 출력
 첫 줄에 네개의 직사각형이 차지하는 면적을 출력한다.
 '''
+
+paper = [[0 for _ in range(101)] for _ in range(101)]
+
+for _ in range(4):
+    x1, y1, x2, y2 = map(int, input().split())
+    
+    for i in range(x1, x2):
+        for j in range(y1, y2):
+            paper[i][j] = 1
+
+answer = 0
+for row in paper:
+    answer += sum(row)
+print(answer)
