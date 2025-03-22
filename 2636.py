9d11f403c07b361c00ec554e829b5c3f33803070
조나희 <105144666+Nahhh-j@users.noreply.github.com>
2025-03-22 11:34:31 +0900

    update

--- 2636.py
+++ 2636.py
@@ -23,3 +23,45 @@
 출력
 첫째 줄에는 치즈가 모두 녹아서 없어지는 데 걸리는 시간을 출력하고, 둘째 줄에는 모두 녹기 한 시간 전에 남아있는 치즈조각이 놓여 있는 칸의 개수를 출력한다.
 '''
+
+from collections import deque
+
+dx = [-1, 0, 1, 0]
+dy = [0, -1, 0, 1]
+def find_air():
+    visited = [[False] * c for _ in range(r)]
+
+    q = deque()
+    q.append([0, 0])
+    visited[0][0] = True
+    cnt = 0
+    while q:
+        y, x = q.popleft()
+
+        for i in range(4):
+            ny = y + dy[i]
+            nx = x + dx[i]
+            if 0 <= ny < r and 0 <= nx < c and not visited[ny][nx]:
+                if board[ny][nx] == 0:
+                    visited[ny][nx] = True
+                    q.append([ny, nx])
+                elif board[ny][nx] == 1:
+                    board[ny][nx] = 0
+                    cnt += 1
+                    visited[ny][nx] = True
+    cheese.append(cnt)
+    return cnt
+
+r, c = map(int, input().split())
+board = [list(map(int, input().split())) for _ in range(r)]
+cheese = []
+
+time = 0
+while True:
+    time += 1
+    cnt = find_air()
+    if cnt == 0:
+        break
+
+print(time-1)
+print(cheese[-2])
