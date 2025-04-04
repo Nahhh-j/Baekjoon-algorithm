f461025c440719561fba463b4890033726a21008
조나희 <105144666+Nahhh-j@users.noreply.github.com>
2025-04-04 12:48:03 +0900

    update

--- 2677.py
+++ 2677.py
@@ -25,3 +25,37 @@ EDSAC 연산은 부동소수점 2의 보수 연산이지만, 단순한 정수 �
 각 테스트 케이스에 대해 입력으로 주어진 수를 표현하기 위한 EDSAC 명령어 한 줄을 출력한다. 출력은 opcode 문자 하나와 공백 하나, 음이 아닌 십진수 정수인 피연산자 하나와 공백 하나, 'F' 또는 'D'로 이뤄져 있다. 만약 입력으로 주어진 수가 정확히 17비트로 표현할 수 없는 수라면, 0에 더 가까운 수로 표현한다. (양수라면 내림, 음수라면 올림한다.)
 D가 -1.0 <= D < 1.0 범위의 수가 아니라면 EDSAC 명령어 대신 "INVALID VALUE"를 출력한다.
 '''
+
+n = int(input())
+graph = []
+for _ in range(n):
+  graph.append(list(map(int,input())))
+visited = [[0] * n for _ in range(n)]
+ 
+dx = [-1,0,1,0]
+dy = [0,1,0,-1]
+ 
+def bfs(graph,x,y,cnt):
+  graph[x][y] = 0
+  queue = []
+  queue.append((x,y))
+  while queue :
+    x,y = queue.pop()
+    for i in range(4):
+      nx = x + dx[i]
+      ny = y + dy[i]
+      if 0<=nx<n and  0<=ny<n:
+        if graph[nx][ny] == 1 and visited[nx][ny] == 0:
+          cnt += 1
+          graph[nx][ny] = 0
+          queue.append((nx,ny))
+  return cnt
+cnt = 0
+answer = []
+for a in range(n):
+  for b in range(n):
+    if graph[a][b] == 1 and visited[a][b] == 0:
+      answer.append(bfs(graph,a,b,cnt+1))
+print(len(answer))
+for i in sorted(answer):
+  print(i)
