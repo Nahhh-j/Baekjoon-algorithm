dcabd693d9fc4aaf25f9418f1770e6a3acb89b1c
조나희 <105144666+Nahhh-j@users.noreply.github.com>
2025-04-10 18:13:14 +0900

    update

--- 2775.py
+++ 2775.py
@@ -12,3 +12,14 @@
 출력
 각각의 Test case에 대해서 해당 집에 거주민 수를 출력하라.
 '''
+
+t = int(input())
+
+for _ in range(t):  
+    floor = int(input())
+    num = int(input())
+    f0 = [x for x in range(1, num+1)]
+    for k in range(floor):
+        for i in range(1, num):
+            f0[i] += f0[i-1]
+    print(f0[-1])
