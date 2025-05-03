725c226deff35ea693bc7ca9a8b8cf8309936b04
조나희 <105144666+Nahhh-j@users.noreply.github.com>
2025-05-03 15:36:05 +0900

    update

--- 1091.py
+++ 1091.py
@@ -17,3 +17,34 @@
 출력
 첫째 줄에 몇 번 섞어야 하는지 출력한다. 만약, 섞어도 섞어도 카드를 해당하는 플레이어에게 줄 수 없다면, -1을 출력한다.
 '''
+
+import sys
+from collections import defaultdict
+def input(): return sys.stdin.readline().rstrip()
+
+n = int(input())
+P = list(map(int,input().split()))
+S = list(map(int,input().split()))
+
+answer = defaultdict(set)
+visited = set()
+
+for i in range(n):
+    answer[P[i]].add(i)
+
+cnt = 0
+cards = list(range(n))
+while True:
+    first,second,third = set(cards[0:n:3]), set(cards[1:n:3]), set(cards[2:n:3])
+    if first == answer[0] and second == answer[1] and third == answer[2]:
+        break
+    if tuple(cards) in visited:
+        cnt = -1
+        break
+    visited.add(tuple(cards))
+    temp = [0] * n
+    for i in range(n):
+        temp[S[i]] = cards[i]
+    cards = temp
+    cnt += 1
+print(cnt)
