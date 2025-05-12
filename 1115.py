f38530af2e049a28dd7fe8c3063bdbfa01006b9d
조나희 <105144666+Nahhh-j@users.noreply.github.com>
2025-05-12 19:28:36 +0900

    update(java)

--- 1115.py
+++ 1115.py
@@ -25,3 +25,32 @@ A	B
 출력
 첫째 줄에 입력으로 주어진 순열 P와 차이가 가장 작은 완벽한 순열 Q의 차이를 출력한다.
 '''
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    private void solution() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in), 1<<16);
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) arr[i] = Integer.parseInt(st.nextToken());
+        boolean[] v = new boolean[n];
+        int cnt = 0;
+        for (int i = 0; i < n; i++) {
+            if (v[i]) continue;
+            cnt++;
+            int s = i;
+            while (!v[s]) {
+                v[s] = true;
+                s = arr[s];
+            }
+        }
+        System.out.println(cnt==1?0:cnt);
+    }
+    public static void main(String[] args) throws Exception {
+        new Main().solution();
+    }
+}
