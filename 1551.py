cbb16cdcc9a14148df979230647fe06b4ddd11ad
조나희 <105144666+Nahhh-j@users.noreply.github.com>
2025-06-02 09:02:09 +0900

    update

--- 1551.py
+++ 1551.py
@@ -12,3 +12,10 @@
 출력
 첫째 줄에 K번 변형한 수열을 ‘,’로 구분하여 출력한다.
 '''
+
+N, K = map(int, input().split())
+li = list(map(int, input().split(',')))
+for _ in range(K):
+    t = [li[i+1]-li[i] for i in range(len(li)-1)]
+    li = t
+print(*li, sep=',')
