efb86dca3d3a50c9f15a9c83af10e49562a7276f
조나희 <105144666+Nahhh-j@users.noreply.github.com>
2025-06-12 09:32:26 +0900

    update

--- 1515.py
+++ 1515.py
@@ -13,3 +13,16 @@
 출력
 가능한 N 중에 최솟값을 출력한다.
 '''
+
+import sys
+nums = sys.stdin.readline().rstrip()
+n = 0
+idx = 0
+while True:
+    n += 1
+    for s in str(n):
+        if nums[idx] == s:
+            idx += 1
+            if idx >= len(nums):
+                print(n)
+                exit()
