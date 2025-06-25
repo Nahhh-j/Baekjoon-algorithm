41976afdc301df5d1f1665b8bc0f75cb2d760a65
조나희 <105144666+Nahhh-j@users.noreply.github.com>
2025-06-25 19:58:03 +0900

    update

--- 1862.py
+++ 1862.py
@@ -11,3 +11,17 @@
 출력
 첫째 줄에 원래 달린 거리를 출력한다.
 '''
+
+n = int(input())
+
+length = len(str(n))
+result = 0
+for i in range(length):
+    digit = n%10
+    n = n//10
+
+    if digit >4:
+        result += (digit-1) * (9**i)
+    else:
+        result += digit * (9**i)
+print(result)
