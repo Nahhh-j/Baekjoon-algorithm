8da8ba28e4ebbee916516e0169b6a64798859d99
조나희 <105144666+Nahhh-j@users.noreply.github.com>
2025-07-21 08:59:28 +0900

    update

--- 2149.py
+++ 2149.py
@@ -28,3 +28,33 @@ B는 두 가지가 있기 때문에 더 왼쪽에 있었던 (B)MBLR이 먼저 �
 출력
 첫째 줄에 평문을 출력한다.
 '''
+
+n = input()
+k = input()
+
+length = len(k) // len(n)
+arr = []
+
+temp = list(n)
+temp.sort()
+
+for i in range(len(n)):
+  arr.append([temp[i], i, k[length * i : length * i + length]])
+
+arr.sort(key = lambda x : x[1])
+ans = []
+
+while arr:
+  for i in n:
+    for j in range(len(arr)):
+      if arr[j][0] == i:
+        ans.append(arr[j])
+        arr.pop(j)
+        break
+
+result = ''
+for i in range(length):
+  for j in range(len(n)):
+    result += ans[j][2][i]
+
+print(result)
