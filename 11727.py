8d0b28c66ca8e2c05bd926eaf8dd60aaacdfb68c
조나희 <105144666+Nahhh-j@users.noreply.github.com>
2025-08-23 13:38:02 +0900

    update

--- 11727.py
+++ 11727.py
@@ -11,3 +11,17 @@
 출력
 첫째 줄에 2×n 크기의 직사각형을 채우는 방법의 수를 10,007로 나눈 나머지를 출력한다.
 '''
+
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+dp = [0] * 1001
+
+dp[0] = 1
+dp[1] = 1
+
+for i in range(2, n+1):
+    dp[i] = dp[i-1] + 2 * dp[i-2]
+
+print(dp[n]%10007)
