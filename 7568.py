583e41d4255ab65fd6217e4fe0c8622e26931284
조나희 <105144666+Nahhh-j@users.noreply.github.com>
2025-08-26 09:26:06 +0900

    update

--- 7568.py
+++ 7568.py
@@ -23,3 +23,17 @@ E	(46, 155)	5
 2 ≤ N ≤ 50
 10 ≤ x, y ≤ 200
 '''
+
+num_student = int(input())
+student_list = []
+
+for _ in range(num_student):
+    weight, height = map(int, input().split())
+    student_list.append((weight, height))
+
+for i in student_list:
+    rank = 1
+    for j in student_list:
+        if i[0] < j[0] and i[1] < j[1]:
+                rank += 1
+    print(rank, end = " ")
