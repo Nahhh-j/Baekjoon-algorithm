0124ffb7e2c415b8a51a919bba27050ebc9d4293
조나희 <105144666+Nahhh-j@users.noreply.github.com>
2025-09-02 08:43:04 +0900

    update

--- 1389.py
+++ 1389.py
@@ -22,3 +22,32 @@ BOJ 유저의 수와 친구 관계가 입력으로 주어졌을 때, 케빈 베�
 출력
 첫째 줄에 BOJ의 유저 중에서 케빈 베이컨의 수가 가장 작은 사람을 출력한다. 그런 사람이 여러 명일 경우에는 번호가 가장 작은 사람을 출력한다.
 '''
+
+import sys
+INF = int(1e9)
+N, M = map(int,input().split())
+graph =[[INF] * (N + 1)  for _ in range(N + 1)]
+
+for _ in range(M):
+    a,b = map(int,sys.stdin.readline().rstrip().split())
+    graph[a][b] = 1
+    graph[b][a] = 1
+
+for i in range(1, N + 1):
+    for j in range(1, N + 1):
+        if i == j:
+            graph[i][j] = 0
+
+for k in range(1, N + 1):
+    for i in range(1, N + 1):
+        for j in range(1 , N + 1):
+            graph[i][j] = min(graph[i][j],graph[i][k] + graph[k][j])
+
+bacon = INF
+answer = 0
+for i in range(N, 0, -1):
+    s = sum(graph[i][1:])
+    if bacon >= s:
+        bacon = s
+        answer = i
+print(answer)
