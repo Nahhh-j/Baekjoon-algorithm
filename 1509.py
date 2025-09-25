5d71275797a87b07088415b4c31d258ae556545c
조나희 <105144666+Nahhh-j@users.noreply.github.com>
2025-09-25 09:20:29 +0900

    update

--- 1509.py
+++ 1509.py
@@ -11,3 +11,39 @@
 출력
 첫째 줄에 팰린드롬 분할의 개수의 최솟값을 출력한다.
 '''
+
+import sys
+sys.setrecursionlimit(10**8)
+input = sys.stdin.readline
+
+
+def palindrome():
+    for i in range(L):
+        is_p[i][i] = 1
+    
+    for i in range(1,L):
+        if string[i-1] == string[i]:
+            is_p[i-1][i] = 1
+    
+    for l in range(3, L+1):
+        for start in range(L-l+1):
+            end = start + l -1
+            if string[start] == string[end] and is_p[start+1][end-1]:
+                is_p[start][end] = 1
+    
+    for end in range(L):
+        for start in range(end+1):
+            if is_p[start][end]:
+                dp[end] = min(dp[end], dp[start-1] + 1)
+            else:
+                dp[end] = min(dp[end], dp[end-1] + 1)
+
+if __name__ == "__main__":
+    string = input().strip()
+    L = len(string)
+    dp = [2500 for _ in range(L+1)]
+    dp[-1] = 0
+    is_p = [[0 for _ in range(L)] for _ in range(L)]
+    
+    palindrome()
+    print(dp[L-1])
