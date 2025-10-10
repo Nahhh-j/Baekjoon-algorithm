8aab7bdf16872df0c89d351385b6e96551104aa4
조나희 <105144666+Nahhh-j@users.noreply.github.com>
2025-10-10 09:11:32 +0900

    update

--- 1949.py
+++ 1949.py
@@ -16,3 +16,30 @@ N개의 마을로 이루어진 나라가 있다. 편의상 마을에는 1부터 
 출력
 첫째 줄에 '우수 마을'의 주민 수의 총 합을 출력한다.
 '''
+
+import sys
+sys.setrecursionlimit(10**6)
+N = int(sys.stdin.readline())
+people = [0] + list(map(int, sys.stdin.readline().split()))
+tree = [[] for _ in range(N+1)]
+dp = [[0, 0] for _ in range(N+1)]
+visit = [False] * (N+1)
+
+for _ in range(N-1):
+    a, b = map(int, sys.stdin.readline().split())
+    tree[a].append(b)
+    tree[b].append(a)
+
+
+def dfs(node):
+    visit[node] = True
+
+    dp[node][1] += people[node]
+    for child in tree[node]:
+        if not visit[child]:
+            dfs(child)
+            dp[node][0] += max(dp[child][0], dp[child][1])
+            dp[node][1] += dp[child][0]
+
+dfs(1)
+print(max(dp[1]))
