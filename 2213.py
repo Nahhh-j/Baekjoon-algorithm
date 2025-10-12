95d83056515077a38f826a16ded9a309eed534ee
조나희 <105144666+Nahhh-j@users.noreply.github.com>
2025-10-12 21:46:24 +0900

    update

--- 2213.py
+++ 2213.py
@@ -11,3 +11,49 @@
 출력
 첫째 줄에 최대 독립집합의 크기를 출력한다. 둘째 줄에는 최대 독립집합에 속하는 정점을 오름차순으로 출력한다. 최대 독립 집합이 하나 이상일 경우에는 하나만 출력하면 된다.
 '''
+
+import sys
+N = int(sys.stdin.readline())
+tree = [[] for _ in range(N+1)]
+dp = [[0, 0] for _ in range(N+1)]
+path = [[[] for _ in range(2)] for _ in range(N+1)]
+W = [0] + list(map(int, sys.stdin.readline().split()))
+visit = [False] * (N+1)
+
+for _ in range(N-1):
+    A, B = map(int, sys.stdin.readline().split())
+    tree[A].append(B)
+    tree[B].append(A)
+
+
+def dfs(node):
+    visit[node] = True
+    dp[node][1] += W[node]
+    path[node][1].append(node)
+
+    for x in tree[node]:
+        if not visit[x]:
+            result = dfs(x)
+            dp[node][0] += max(dp[x][0], dp[x][1])
+            dp[node][1] += dp[x][0]
+
+            path[node][1] += result[0]
+            if dp[x][0] > dp[x][1]:
+                path[node][0] += result[0]
+            else:
+                path[node][0] += result[1]
+
+    return path[node]
+
+
+p = dfs(1)
+if dp[1][0] > dp[1][1]:
+    print(dp[1][0])
+    p[0].sort()
+    for i in p[0]:
+        print(i, end=' ')
+else:
+    print(dp[1][1])
+    p[1].sort()
+    for i in p[1]:
+        print(i, end=' ')
