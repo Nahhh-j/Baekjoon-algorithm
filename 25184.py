23b1120c0623f4988d10bd5bb8924595c3b23326
조나희 <105144666+Nahhh-j@users.noreply.github.com>
2025-10-20 00:41:15 +0900

    update

--- 25184.py
+++ 25184.py
@@ -16,3 +16,36 @@ $\lfloor \frac{N}{2} \rfloor$이상이다.
 출력
 첫째 줄에 길이가 $N$인 동가수열을 아무거나 하나 출력한다. 동가수열은 공백으로 구분해서 출력해야 한다.
 '''
+
+import java.io.*;
+
+public class C0032
+{
+    public static void main (String[] args) throws IOException
+    {
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter output = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder s = new StringBuilder("");
+        int N = Integer.parseInt(input.readLine());
+        int mid = N/2; 
+
+        if ( N == 1 )
+        {
+            output.write("1");
+            output.close();
+            return;
+        }
+
+        for (int i=1; i<=mid; i++)
+        {
+            s.append(mid + i + " ");
+            s.append(i + " ");
+        }
+
+        if ( N % 2 == 1 )
+            s.append(N + "");
+
+        output.write(s.toString());
+        output.close();
+    }
+}
