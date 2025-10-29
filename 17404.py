e7d876de0c163a6f5c1d0d9597f19330e4ba8249
조나희 <105144666+Nahhh-j@users.noreply.github.com>
2025-10-29 09:28:57 +0900

    update

--- 17404.py
+++ 17404.py
@@ -15,3 +15,22 @@ i(2 ≤ i ≤ N-1)번 집의 색은 i-1, i+1번 집의 색과 같지 않아야 �
 출력
 첫째 줄에 모든 집을 칠하는 비용의 최솟값을 출력한다.
 '''
+
+import sys
+
+N = int(sys.stdin.readline().rstrip())
+houses = [[-1, -1, -1]]
+for _ in range(N):
+    houses.append(list(map(int, sys.stdin.readline().rstrip().split())))
+
+R, G, B = 0, 1, 2
+
+dp = [[-1] * 3 for _ in range(N+1)]
+dp[1] = houses[1]
+
+for i in range(2, N+1):
+    dp[i][R] = min(dp[i-1][G], dp[i-1][B]) + houses[i][R]
+    dp[i][G] = min(dp[i-1][R], dp[i-1][B]) + houses[i][G]
+    dp[i][B] = min(dp[i-1][R], dp[i-1][G]) + houses[i][B]
+
+print(min(dp[N]))
