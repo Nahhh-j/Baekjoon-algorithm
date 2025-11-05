4dcddca917ddb9a8e62d366086e2aef3487c1599
조나희 <105144666+Nahhh-j@users.noreply.github.com>
2025-11-05 09:22:22 +0900

    update

--- 1535.py
+++ 1535.py
@@ -12,3 +12,22 @@
 출력
 첫째 줄에 세준이가 얻을 수 있는 최대 기쁨을 출력한다.
 '''
+
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+L = list(map(int, input().split()))
+J = list(map(int, input().split()))
+L, J = [0] + L, [0] + J
+dp = [[0 for _ in range(101)] for _ in range(N+1)]
+
+for i in range(1, N+1):
+    for j in range(1, 101):
+        if L[i] <= j:
+            dp[i][j] = max(dp[i-1][j], dp[i-1][j-L[i]] + J[i])
+        else:
+            dp[i][j] = dp[i-1][j]
+
+print(dp[N][99])
