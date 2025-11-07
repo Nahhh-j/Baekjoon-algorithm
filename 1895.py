913c81170472fdac63244f6e53eccffa8a3219ec
조나희 <105144666+Nahhh-j@users.noreply.github.com>
2025-11-07 09:22:56 +0900

    update

--- 1895.py
+++ 1895.py
@@ -20,3 +20,29 @@
 출력
 첫째 줄에 필터링 된 이미지 J의 각 픽셀 값 중에서 T보다 크거나 같은 것의 개수를 출력한다.
 '''
+
+R, C = map(int, input().split())
+
+Filter = []
+for r in range(R) :
+    Filter.append(list(map(int,input().split())))
+
+T = int(input())
+J = []
+
+for r in range(R-3+1) :
+    for c in range(C-3+1) :
+        median = []
+        for i in range(r, r+3) :
+            for j in range(c, c+3) :
+                median.append(Filter[i][j])
+
+        median.sort()    
+        J.append(median[4])
+
+cnt = 0
+for k in J:
+    if k > T or k == T :
+        cnt += 1
+
+print(cnt)
