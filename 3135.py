0cbc95edec96f116b63055b57d789abf2dd7cb0e
조나희 <105144666+Nahhh-j@users.noreply.github.com>
2025-12-03 18:39:11 +0900

    update

--- 3135.py
+++ 3135.py
@@ -21,3 +21,19 @@
 출력
 주파수 A에서 B로 갈 때 눌러야 하는 버튼수의 최솟값을 출력한다.
 '''
+
+A, B = map(int, input().split())
+N = int(input())
+
+arr = []
+[arr.append(int(input())) for _ in range(N)]
+
+min_value = 1001
+idx = 0
+for i in arr:
+    temp = abs(i - B)
+    if min_value > temp:
+        min_value = temp
+        idx = i
+
+print(min(abs(A-B), (abs(idx-B)+1)))
