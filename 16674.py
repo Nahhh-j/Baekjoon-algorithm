09bebd091aca088d6d273951604499c3d6bf00c9
조나희 <105144666+Nahhh-j@users.noreply.github.com>
2026-01-08 09:29:07 +0900

    update

--- 16674.py
+++ 16674.py
@@ -13,3 +13,16 @@ N은 0으로 시작하지 않는다.
 출력
 첫 번째 줄에 N이 2018과 관련 없는 수이면 0을, 2018과 관련 있는 수이나 밀접한 수가 아니면 1을, 2018과 밀접한 수이나 묶여있는 수가 아니면 2를, 2018과 묶여있는 수이면 8을 출력한다.
 '''
+
+N = list(map(int, input()))
+
+if (N.count(2) + N.count(0) + N.count(1) + N.count(8)) == len(N):
+    if (2 in N) and (0 in N) and (1 in N) and (8 in N):
+        if N.count(2) == N.count(0) == N.count(1) == N.count(8):
+            print(8)
+        else:
+            print(2)
+    else:
+        print(1)
+else:
+    print(0)
