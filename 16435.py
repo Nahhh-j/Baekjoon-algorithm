0aa10d2ef8e4d7d950b27956ef91e77df86e71b4
조나희 <105144666+Nahhh-j@users.noreply.github.com>
2026-01-24 14:07:45 +0900

    update

--- 16435.py
+++ 16435.py
@@ -15,3 +15,14 @@
 출력
 첫 번째 줄에 스네이크버드의 최대 길이를 출력합니다.
 '''
+
+import sys
+n, l = map(int, sys.stdin.readline().strip().split())
+h = list(map(int, sys.stdin.readline().strip().split()))
+h.sort()
+for i in range(n):
+    if(l >= h[i]):
+        l += 1
+    else:
+        break;
+print(l)
