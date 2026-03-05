aa8b05be626613a61341421ba2fb9ac32c4e9f00
조나희 <105144666+Nahhh-j@users.noreply.github.com>
2026-03-05 19:14:30 +0900

    update

--- 12101.py
+++ 12101.py
@@ -29,3 +29,30 @@
 출력
 n을 1, 2, 3의 합으로 나타내는 방법 중에서 사전 순으로 k번째에 오는 것을 출력한다. k번째 오는 식이 없는 경우에는 -1을 출력한다.
 '''
+
+import sys
+input = sys.stdin.readline
+
+def dfs(nums, n, numbers):
+    sumNums = sum(nums)
+    if sumNums == n:
+        numbers.append(nums)
+    elif sumNums > n:
+        return
+    if len(nums) > n:
+        return
+    for i in range(1, 4):
+        dfs(nums + [i], n, numbers)
+
+def main():
+    n, k = map(int, input().split())
+    numbers = []
+    dfs([], n, numbers)
+    if len(numbers) < k:
+        print('-1')
+    else:
+        print('+'.join(map(str, numbers[k-1])))
+    return
+
+if __name__ == "__main__":
+    main()
