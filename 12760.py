30184204ecf03f83c2844610ff393a0793b3e366
조나희 <105144666+Nahhh-j@users.noreply.github.com>
2026-03-14 12:21:19 +0900

    update

--- 12760.py
+++ 12760.py
@@ -10,3 +10,46 @@
 출력
 최종 승자의 번호를 출력한다. 플레이어의 번호는 1번부터 \(N\)번까지 입력받은 순서로 주어진다고 가정한다. 가장 많은 점수를 획득한 플레이어가 여러 명일 경우, 빈칸을 사이에 두고 플레이어들의 번호를 오름차순으로 출력한다.
 '''
+
+import sys
+
+N, M = map(int, sys.stdin.readline().split())
+
+player_history = dict()
+maximun_card_list = [0] * M
+
+for i in range(1, N + 1):
+    card_list = sorted(map(int, sys.stdin.readline().split()), key=lambda x: -x)
+
+    player_history[i] = {
+        'score': 0,
+        'card_list': card_list
+    }
+
+    for j in range(M):
+        
+        if maximun_card_list[j] < card_list[j]:
+            maximun_card_list[j] = card_list[j]
+
+maximun_score = 0
+
+for player, v in player_history.items():
+    
+    for k in range(M):
+        
+        if maximun_card_list[k] != v['card_list'][k]:
+            continue
+        
+        player_history[player]['score'] += 1
+
+        if maximun_score < player_history[player]['score']:
+            maximun_score = player_history[player]['score']
+
+best_player_list = []
+
+for player, v in player_history.items():
+    
+    if maximun_score == v['score']:
+        best_player_list.append(str(player))
+
+print(' '.join(best_player_list))
